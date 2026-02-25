1eae3fb86b1eb222a3e2524f1ef82ac444ef5d06
Sara260607 <saratoure72@gmail.com>
2026-02-25 11:04:43 -0500

    fix: resolve merge conflict and restore French phrase in app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 print("Hello, World!")
-def greet(name):
-    return f"Hello, {name}!"
 print("Welcome to the world of git.")
 print("This is a simple Python application.")
-<<<<<<< HEAD
-print("Texte d'essaye.")
-=======
 print("la vie est belle" )
->>>>>>> f823cce0d9202989f569b197ddd0a5a652c3147d
