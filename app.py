1aacb4ddb5bf815b89cff4bb44c36ec55981ccca
boukhrisa952-max <boukhrisa952@gmail.com>
2026-02-25 11:12:45 -0500

    git push

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-print("Hello, World!")
+print("Hello, World")
+
 def greet(name):
     return f"Hello, {name}!"
-print("Welcome to the world of git.")
+
+print("Welcome to the world of git..")
 print("This is a simple Python application.")
+print("Texte d’essaye.")
+print("la vie est belle")
