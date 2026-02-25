4156182c3b5a28995d99951859ad139a882df643
Sara260607 <saratoure72@gmail.com>
2026-02-25 11:26:54 -0500

    Remove merge conflict markers and clean up print statements in app.py

--- app.py
+++ app.py
@@ -3,19 +3,8 @@ def greet(name):
     return f"Hello, {name}!"
 print("Welcome to the world of git.")
 print("This is a simple Python application.")
-<<<<<<< HEAD
 print("Texte d'essaye.")
-=======
-print("la vie est belle" )
-<<<<<<< HEAD
->>>>>>> f823cce0d9202989f569b197ddd0a5a652c3147d
-=======
-<<<<<<< HEAD
->>>>>>> f823cce0d9202989f569b197ddd0a5a652c3147d
-
 def add(a, b):
     return a + b
-=======
 print("sara")
->>>>>>> 36822ff5a386b99fa81acd2bcc70b45e908a673c
->>>>>>> 44e188620d552f43ac9a222cdad7a6cc4d2c7aa7
+
