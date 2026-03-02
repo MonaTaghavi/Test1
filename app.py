445de05205ddabe606865ac08bf361d6772a9a0b
MonaTaghavi <mona.taghavi@collegelasalle.com>
2026-03-02 14:24:53 -0500

    feat: add sum and multiply functions to app.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ print("Welcome to the world of git.")
 print("This is a simple Python application.")
 print("Texte d'essaye.")
 My_variable = "This is a variable."
+def sum(a, b):
+    return a + b
+def multiply(a, b):
+    return a * b
