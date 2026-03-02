57cfaeac7ed62e19a0c420f4a3ae58aa7da1fb61
MonaTaghavi <mona.taghavi@collegelasalle.com>
2026-03-02 10:34:44 -0500

    new

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ def greet(name):
     return f"Hello, {name}!"
 print("Welcome to the world of git.")
 print("This is a simple Python application.")
+<<<<<<< HEAD
+print("Texte d'essaye.")
+=======
 print("Texte d'essaye.")
 print("la vie est belle" )
 
 def add(a, b):
     return a + b
+
+>>>>>>> 36822ff5a386b99fa81acd2bcc70b45e908a673c
