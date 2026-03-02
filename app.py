8f323d3ef06d6b071cce4f1569dd6c1291b23db2
MonaTaghavi <mona.taghavi@collegelasalle.com>
2026-03-02 10:59:13 -0500

    fix: remove merge conflict markers and clean up print statements in app.py

--- app.py
+++ app.py
@@ -4,11 +4,5 @@ def greet(name):
 print("Welcome to the world of git.")
 print("This is a simple Python application.")
 print("la vie est belle" )
-<<<<<<< HEAD
->>>>>>> f823cce0d9202989f569b197ddd0a5a652c3147d
-
 def add(a, b):
     return a + b
-=======
-print("sara")
->>>>>>> 36822ff5a386b99fa81acd2bcc70b45e908a673c
